52124634c94a45ce83fdcdd4f89f3f4c547654ec
12jaime12 <12jaimejcc12@gmail.com>
2023-04-27 17:16:29 +0200

    rickandmorty

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css";
 
 const template = () => {
   return `
-    <img src="https://1000marcas.net/wp-content/uploads/2022/04/Rick-and-Morty.png"/>
+    <img src="https://1000marcas.net/wp-content/uploads/2022/04/Rick-and-Morty.png" alt="logo"/>
     `;
 };
 
